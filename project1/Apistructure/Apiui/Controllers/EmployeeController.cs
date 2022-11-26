ba984cbf8e1ca6438bb3bdc16bf7f078d253b013
Alexander Laroche <alexanderlaroche@Harolds-iMac.local>
2022-11-26 17:43:46 -0500

    Api rough draft updated

---
+++ project1/Apistructure/Apiui/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelsLayer;
+using BusinessLayer;
+
+namespace Apiui.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly BusinessLayerClass _buslayer = new BusinessLayerClass();
+
+        [HttpPost("reimbursementportal")]
+        public ActionResult<Employee> PostEmployeeController(Employee e)
+        {
+            e.fName = "Antoine";
+            Employee emp1 = _buslayer.PostEmployeeController(e);
+
+            return Created("mydb/employee/here", emp1);
+            
+        }
+
+    }
+}
